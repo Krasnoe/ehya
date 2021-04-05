8d780c2d2914ce4d1b4fc5b131d9a74b053430b8
Krasnoe <psp.tema@mail.ru>
2021-04-05 17:32:53 +0300

    added all files

---
+++ js/main.js
@@ -0,0 +1,119 @@
+$(document).ready(function () {
+
+const menuButton = document.querySelector('.menu-buttons');
+menuButton.addEventListener('click', function () {
+  console.log('кнопка');
+  document.querySelector('.navbar-top__nav').classList.toggle('navbar-top__nav--visible');
+  
+})
+
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: false,
+  spaceBetween: 30,
+  slidesPerView: 4,
+  
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+  
+  keyboard: {
+    enabled: false,
+    onlyInViewport: false,
+  },
+
+});
+
+$(".regular-slider").slick({
+        dots: false,
+        infinite: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        centerMode: false,
+        responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 0.5,
+          }
+        },
+        {
+          breakpoint: 993,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 0.75,
+          }
+        },
+        {
+          breakpoint: 850,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            centerMode: true,
+          }
+        },
+        {
+          breakpoint: 410,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        },
+      ]
+      });
+
+  const modalOverlay = $('.modal__overlay');
+  const modalDialog = $('.modal__dialog');
+  const modalButton = $('[data-toggle="modal"]');
+  const closeModalButton = $('.modal__close');
+  const closeModalOverlay = $('.modal__overlay');
+  
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal);
+  closeModalOverlay.on('click', closeModal);
+
+  function openModal() {
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'Escape') {
+      closeModal(event);
+    };
+  });
+
+  // Обработка форм
+  $(".modal__form").validate({
+      errorClass: "form-1",
+      messages: {
+        name: {
+            required: "Пожалуйста, укажите имя",
+            minlength: "Имя должно быть длиннее 2 букв",
+          },
+          email: {
+              required: "Ваша почта для связи",
+              email: "Еmail должен быть в формате name@domain.ru"
+            },
+            phone: {
+                required: "Номер телефона обязателен",
+                minlength: "Номер должен быть из 10 цифр"
+              },
+            }
+            });
+            
+  
+$(".phone").mask("+7 (000) 000-0000");
+  
+  AOS.init();
+
+})
---
+++ send.php
@@ -0,0 +1,75 @@
+<?php
+include 'phpmailer/password.php';
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$message = $_POST['message'];
+$email = $_POST['email'];
+
+// Формирование самого письма
+if (count($_POST) == 1) {
+    $title = "Новое обращение Ehya";
+    $body = "<b>Почта для подписки:</b> $email";
+} 
+else if (count($_POST) == 3) {
+    $title = "Новое обращение Ehya";
+    $body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br>
+<b>Сообщение:</b><br>$message<br>
+";
+}
+else if (count($_POST) == 4) {
+    $title = "Новое обращение Ehya";
+    $body = "
+<h2>Новое обращение</h2>
+<b>Имя:</b> $name<br>
+<b>Телефон:</b> $phone<br>
+<b>Электронная почта:</b> $email<br>
+<b>Сообщение:</b><br>$message<br>
+";
+}
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.yandex.ru'; // SMTP сервера вашей почты
+    $mail->Username   = 'thekrasnoe@yandex.ru'; // Логин на почте
+    $mail->Password   = $password; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('thekrasnoe@yandex.ru', 'thekrasnoe'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('thekrasnoe@yandex.ru');
+
+    // Отправка сообщения
+    $mail->isHTML(true);
+    $mail->Subject = $title;
+    $mail->Body = $body;
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
+?>
